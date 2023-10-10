664bde910d74719b9cf14e8faf8b10f69da1389e
krystiano13 <crystianotv@gmail.com>
2023-10-10 12:49:04 +0200

    create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCss = require('gulp-clean-css');
+
+const styles = () => {
+    return src('./**/*.scss')
+        .pipe(sass())
+        .pipe(cleanCss())
+        .pipe(dest('./'));
+}
+
+const watcher = () => {
+    return watch(['./**/*.scss'], styles);
+}
+
+exports.default = series(watcher, styles);
